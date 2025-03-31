a3c0336e82bf549f717cc54f9477a0e0fdb7b88b
Lucas Ruiz <lucasruizserre13@gmail.com>
2025-03-31 18:21:16 -0300

    primera parte de js

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// PRODUCTOS
+const productosArray = [
+    // MATES
+    {
+        id: "mates-01",
+        titulo: "Mate 01",
+        imagen: "./img/mates/mate1.jpg",
+        categoria: {
+            nombre: "Mates",
+            id: "mates",
+        },
+        precio: 1000
+    },
+    {
+        id: "mates-02",
+        titulo: "Mate 02",
+        imagen: "./img/mates/mate2.jpg",
+        categoria: {
+            nombre: "Mates",
+            id: "mates",
+        },
+        precio: 1000
+    },
+    // TERMOS
+    {
+        id: "termo-01",
+        titulo: "Termo 01",
+        imagen: "./img/mates/termo.jpg",
+        categoria: {
+            nombre: "Termos",
+            id: "termos",
+        },
+        precio: 1000
+    },
+    // ACCESORIOS
+    {
+        id: "matera-01",
+        titulo: "Matera 01",
+        imagen: "./img/mates/matera1.jpg",
+        categoria: {
+            nombre: "Matera",
+            id: "matera",
+        },
+        precio: 1000
+    }
+]
